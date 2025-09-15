87541951600f566182813730717eaa2eeb941b6e
Lucca <219886457+Engineer-Lucca@users.noreply.github.com>
2025-09-14 22:43:57 -0300

    Add files via upload

---
+++ Trabalho Email Phishing.py
@@ -0,0 +1,16 @@
+# Função para verificar se o corpo do e-mail contém palavras suspeitas de phishing
+def verificar_phishing(mensagem):
+    # Lista de palavras que indicam possíveis e-mails de phishing
+    palavras_suspeitas = ["ganhe", "prêmio", "urgente", "desconto", "click", "promoção"]
+    
+    # Verifique se alguma palavra suspeita está presente no corpo do e-mail:
+    mensagem_lower = mensagem.lower()
+    for palavra in palavras_suspeitas:
+        if palavra in mensagem_lower:
+            return "Phishing"
+    return "Seguro"
+
+email_usuario = input()
+email_usuario = email_usuario.strip()
+resultado = verificar_phishing(email_usuario)
+print(f"Classificação: {resultado}")
